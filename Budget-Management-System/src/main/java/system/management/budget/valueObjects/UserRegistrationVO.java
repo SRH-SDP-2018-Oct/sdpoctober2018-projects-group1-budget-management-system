d2b93ef40acf3d5ca2c6cace4c7efc3f633c37aa
SanketWarambhe <sanketwarambhe@gmail.com>
2018-11-27 00:26:39 +0100

    Uploading the complete project
    Contains login and user registration logic

---
+++ Budget-Management-System/src/main/java/system/management/budget/valueObjects/UserRegistrationVO.java
@@ -0,0 +1,32 @@
+package system.management.budget.valueObjects;
+
+public class UserRegistrationVO {
+	private String firstName;
+	private String lastName;
+	private String emailID;
+	private String mobileNumber;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmailID() {
+		return emailID;
+	}
+	public void setEmailID(String emailID) {
+		this.emailID = emailID;
+	}
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+}
